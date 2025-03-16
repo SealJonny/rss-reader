93ae5db406def687feebe11f7167d011da354737
SealJonny <jon.fri14@gmail.com>
2025-03-16 01:48:38 +0100

    fix(): check if feed.id is present

--- src/database/db-jobs.ts
+++ src/database/db-jobs.ts
@@ -4,11 +4,9 @@ import db from "./database";
 export async function insertAllNews() {
   const feeds = await db.rssFeeds.all();
   feeds.forEach(async f => {
-    const news = await fetchRss(f.link);
+    const news = await fetchRss(f);
     if (news === null) {
       throw new Error(`Error while fetching news for ${f.link}`);
     }
-
-
   });
 }
--- src/xml/rss.ts
+++ src/xml/rss.ts
@@ -1,9 +1,9 @@
 import { JSDOM } from "jsdom";
 import { NewsItem } from "../interfaces/news-item";
-import { RssFeedEmptyError } from "../errors/rss-feed";
+import { RssFeedEmptyError, RssFeedInvalidError } from "../errors/rss-feed";
 import { RssFeed } from "../interfaces/rss-feed";
 
-function parseDescription(data: string | null | undefined): string | null {
+function extractDescription(data: string | null | undefined): string | null {
   if (!data) return null;
 
   let dom = new JSDOM(data);
@@ -27,47 +27,45 @@ function parseDescription(data: string | null | undefined): string | null {
 }
 
 export async function fetchRss(feed: RssFeed): Promise<NewsItem[] | null> {
-    try {
-        const response = await fetch(feed.link);
-        const text = await response.text();
-        const dom = new JSDOM(text, { contentType: "text/xml" });
+  if (typeof feed.id === "undefined") {
+    throw new RssFeedInvalidError("RssFeed does not contain a valid id!", feed.link);
+  }
 
-        // Query all news in xml
-        const channel = dom.window.document.querySelector("channel");
-        if (!channel) {
-          throw new RssFeedEmptyError("No channel tag found in response!", feed.link);
-        }
-        const items = channel.querySelectorAll("item");
-        let news: NewsItem[] = [];
-        items.forEach(item => {
-            const link = item.querySelector("link")?.textContent || "";
-            const title = item.querySelector("title")?.textContent || "";
-            const source = item.querySelector("source")?.getAttribute("url") || null;
-            const pubDate = item.querySelector("pubDate")?.textContent || null;
+  try {
+    const response = await fetch(feed.link);
+    const text = await response.text();
+    const dom = new JSDOM(text, { contentType: "text/xml" });
 
-            const descriptionXml = item.querySelector("description");
-            const description = parseDescription(descriptionXml?.textContent) ?? descriptionXml?.textContent ?? "";
-
-            news.push({
-              title: title,
-              link: link,
-              description: description,
-              creationDate: 0,
-              hash: "",
-              isFavorite: false,
-              source: source,
-              pubDate: pubDate,
-              rssFeedId: feed.id!
-            });
-        });
-
-        return news;
-    } catch (error) {
-      return null;
+    const channel = dom.window.document.querySelector("channel");
+    if (!channel) {
+      throw new RssFeedEmptyError("No channel tag found in response!", feed.link);
     }
-}
+    const items = channel.querySelectorAll("item");
+    let news: NewsItem[] = [];
+    items.forEach(item => {
+        const link = item.querySelector("link")?.textContent || "";
+        const title = item.querySelector("title")?.textContent || "";
+        const source = item.querySelector("source")?.getAttribute("url") || null;
+        const pubDate = item.querySelector("pubDate")?.textContent || null;
 
-fetchRss({title: "h", description: "", language: "", lastBuildDate: 0,
-  link: "https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml" }).then(l => console.log(l));
-//fetchRss({title: "h", description: "", language: "", lastBuildDate: 0,
-//  link: "https://news.google.com/rss/search?q=Technology&hl=de&gl=DE&ceid=DE:de" }).then(l => console.log(l));
+        const descriptionXml = item.querySelector("description");
+        const description = extractDescription(descriptionXml?.textContent) ?? descriptionXml?.textContent ?? "";
+
+        news.push({
+          title: title,
+          link: link,
+          description: description,
+          creationDate: 0,
+          hash: "",
+          isFavorite: false,
+          source: source,
+          pubDate: pubDate,
+          rssFeedId: feed.id!
+        });
+    });
+
+    return news;
+  } catch (error) {
+    return null;
+  }
+}
